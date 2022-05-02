606db7d7490f7fef82f92b766388db8b761c2edd
meshtatsuo <meshtatsuo@gmail.com>
2022-05-02 12:19:48 -0600

    added manifest and webpack bundle

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const webpack = require("webpack");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: {
+    app: "./assets/js/index.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: `${__dirname}/public/js`,
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+    }),
+    new WebpackPwaManifest({
+      name: "Budget Tracker",
+      short_name: "Budgeter",
+      background_color: "#ffffff",
+      theme_color: "#ffffff",
+      start_url: "/",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("./public/icons/icon-512x512.png"),
+          sizes: [72, 96, 128, 144, 152, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
+  mode: "development",
+};
+
+module.exports = config;
